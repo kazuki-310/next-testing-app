37530ebffe164db333afaf36e871374aac2bed7c
kazuki-310 <kazu.0310123@gmail.com>
2025-02-11 20:19:45 +0900

    add: カウンターコンポーネントを追加

---
+++ app/counter/Counter.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 10);
+    }, 10000);
+
+    // コンポーネントのクリーンアップ時にインターバルをクリア
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className='m-12'>
+      <h2>カウント: {count}</h2>
+
+      <div className='flex gap-4'>
+        <button className='border border-gray-300 bg-gray-100 p-2 px-4 rounded' onClick={() => setCount(count + 1)}>
+          プラス
+        </button>
+        <button className='border border-gray-300 bg-gray-100 p-2 px-4 rounded' onClick={() => setCount(count - 1)}>
+          マイナス
+        </button>
+      </div>
+    </div>
+  );
+}
